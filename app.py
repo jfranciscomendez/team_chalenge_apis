d691cc4753009f07f87133705e0aff1926c971bf
cristiancabezas <cabezasdelgadocristian@gmail.com>
2025-07-27 18:17:11 +0200

    cambios pre despliegue rama cristian

--- app.py
+++ app.py
@@ -24,19 +24,19 @@ def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
     ram_gb = args.get('ram_gb', None)
     weight = args.get('weight_kg', None)
     inches = args.get('inches', None)
-
+    # con estos prints controlamos lo que se hace internamente
     print(ram_gb,weight,inches)
     print(type(ram_gb))
-
+    # en caso de que no este introducidos los datos que necesita saltara un error
     if ram_gb is None or weight is None or inches is None:
         return render_template('predict.html', title = 'predict', data = False, faltan_datos = True)
-    else:
+    else: # de otra forma, se ejecuta para que salga el valor predicho, hay que hacer unos cambios en las variable spara controlar ese tipo de error al introducir los datos
         ram_gb = float(ram_gb.replace(',', '.'))
         weight = float(weight.replace(',', '.'))
         inches = float(inches.replace(',', '.'))
         prediction = model.predict([[float(ram_gb),float(weight),float(inches)]])
         prediction = round(prediction[0], 2)
-        try:
+        try:# prueba a usar los datos para sacar el RMSE, asi el cliente sabe con que error se le predice el precio
             csv_path = 'data/trainable.csv'   
             data = pd.read_csv(csv_path)
 
@@ -52,9 +52,9 @@ def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
             'resultado':prediction,
             'rmse':rmse
         }
-    return render_template('predict.html', title = 'predict', data = data)
+    return render_template('predict.html', title = 'predict', data = data) # finalemnte si todo ha ido bien, enseña por pantalla el precio
 
-
+# Preguntamos el status, para saber si todo esta bien, necesitamos codigo 200
 @app.route('/status', methods = ['GET'])
 def status():
     data = {
@@ -63,7 +63,7 @@ def status():
     return render_template('status.html', data = data), 200
 
 
-
+# si queremos reentrenar el modelo
 @app.route('/api/v1/retrain/', methods = ['GET'])
 def retrain(): # Ligado al endpoint '/api/v1/retrain/', metodo GET
     csv_path = "tmp/train_new.csv"
@@ -76,29 +76,38 @@ def retrain(): # Ligado al endpoint '/api/v1/retrain/', metodo GET
         model = RandomForestRegressor(max_depth = 5, random_state = 42)
         model.fit(X_train, y_train)
 
-        rmse = round(np.sqrt(mean_squared_error(y_test, model.predict(X_test))),4)
+        rmse = round(np.sqrt(mean_squared_error(y_test, model.predict(X_test))),2)
         mape = round(mean_absolute_percentage_error(y_test, model.predict(X_test)),4)
 
         model.fit(data.drop(columns=['Price_in_euros']), data['Price_in_euros'])
         with open(model_path, 'wb') as f:
             pickle.dump(model, f)
-            
-        return render_template('m_retrain.html') # FALTA PONER EL HTML            #f"Model retrained. New evaluation metric RMSE: {str(rmse)}, MAPE: {str(mape)}"
+        data = {
+            'respuesta': True,
+            'rmse': rmse,
+            'mape': mape
+        }
+        print('todo bien')
+        return render_template('retrain.html', data = data) 
     else:
-        return render_template('m_no_retrain.html') # FALTA PONER EL HTML           #f"<h2>New data for retrain NOT FOUND. Nothing done!</h2>"
+        print('no reentrena')
+        data = {
+            'respuesta' : False
+        }
+        return render_template('retrain.html', data= data) 
     
 
-
+# enseñamos por pantalla las features que tiene nuestro dataset
 @app.route('/features', methods = ['GET'])
 def features():
     return render_template('features.html', title = 'features')
 
-
+# nos devuelve al inicio
 @app.route('/inicio')
 def redireccion():
     return redirect(url_for('index'))
 
-
+# en caso de error navegando por nuestra web, salta error 404
 def error_404(error):
     return render_template('404.html'), 404
 
@@ -106,4 +115,4 @@ def error_404(error):
 
 if __name__ == '__main__':
     app.register_error_handler(404, error_404)
-    app.run(debug=True)  
+    app.run(debug=False)
